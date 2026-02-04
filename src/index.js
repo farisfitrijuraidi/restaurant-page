7e9442369b36b38eccb5ff701e7eb4e705abb9ed
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2026-02-04 20:40:42 +0800

    Implement tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import "./style.css";
-// import { loadHome } from "./home.js";
-// import { loadMenu } from "./menu.js";
+import { loadHome } from "./home.js";
+import { loadMenu } from "./menu.js";
 import { loadContact } from "./contact.js";
-loadContact();
+loadHome();
+
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#menu');
+const contactButton = document.querySelector('#contact');
+const content = document.querySelector('#content');
+
+const clearContent = () => {
+    content.textContent = '';
+}
+
+homeButton.addEventListener('click', () => {
+    clearContent();
+    loadHome();
+})
+
+menuButton.addEventListener('click', () => {
+    clearContent();
+    loadMenu();
+})
+
+contactButton.addEventListener('click', () => {
+    clearContent();
+    loadContact();
+})
